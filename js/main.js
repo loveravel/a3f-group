ca5aaeefed7bd48803179fc5edc49f387ccbe906
Sergei Odintsov <loveravel@yandex.ru>
2019-12-15 06:09:24 +0300

    feat: added a render of the main banner using JS

--- js/main.js
+++ js/main.js
@@ -3,14 +3,17 @@
 const Data = {
   BANNERS: [
     {
+      id: 1,
       title: 'Новая Suzuki Vitara',
       price: '999 000',
       img: 'promo-suzuki-vitara',
     },
     {
+      id: 2,
       title: 'Новая Suzuki SX4',
       price: '1 099 000',
       img: 'promo-suzuki-sx4',
+      defaultOpen: true,
     },
   ],
   CATALOG: {
@@ -133,11 +136,21 @@ const Container = {
   CATALOG_LIST: document.querySelector('.catalog__list'),
 };
 
+/* Tools */
+
 const createElement = (template) => {
   const newElement = document.createElement('div');
   newElement.innerHTML = template;
   return newElement.firstChild;
 };
+
+const doFilterCars = (carsData, modelName) => {
+  return carsData.filter((it) => {
+    return it.name.includes(modelName);
+  });
+}
+
+/* Classes */
 
 class Component {
   constructor() {
@@ -173,13 +186,105 @@ class Component {
   }
 }
 
+class HeroBanner extends Component {
+  constructor(data, count) {
+    super();
+    this._id = data.id;
+    this._count = count;
+    this._title = data.title || '';
+    this._img = data.img || '';
+    this._price = data.price || '';
+
+    this._defaultOpen = data.defaultOpen || false;
+
+    this._onToggleBannerClick = this._onToggleBannerClick.bind(this);
+  }
+
+  _onToggleBannerClick(evt) {
+    const controlID = evt.target.classList.value.match(/\d/);
+
+    document.querySelector(`.hero__banner--${this._id}`).classList.add('hidden');
+    document.querySelector(`.hero__banner--${controlID}`).classList.remove('hidden');
+  }
+
+  _doFormatPrice() {
+    let price = '';
+    const piecesPrice = this._price.split(' ');
+
+    piecesPrice.forEach((it, index, array) => {
+      if (index !== array.length - 1) {
+        price += `${it}<span class="hero__space"> </span>`;
+      } else {
+        price += `${it}`
+      }
+    });
+
+    return price;
+  }
+
+  _getControls() {
+    if (this._count === 1) {
+      return '';
+    }
+
+    let controls = '';
+    for(let i = 1; i <= this._count; i++) {
+      controls += `
+      <button class="hero__control hero__control--${i}${i === this._id ? ' hero__control--active' : ''}">
+        <span class="visually-hidden">Предложение №${i}</span>
+      </button>`;
+    }
+
+    return controls;
+  }
+
+  get template() {
+    return `
+    <div class="hero__banner hero__banner--${this._id} ${this._defaultOpen ? '' : 'hidden'}">
+      <div class="hero__wrapper wrapper">
+        <div class="hero__container">
+          <h2 class="hero__title">${this._title}</h2>
+          <span class="hero__price">от ${this._doFormatPrice()} ₽</span>
+          <a class="hero__button button" href="#">Узнать подробнее</a>
+          <div class="hero__controls">
+            ${this._getControls()}
+          </div>
+        </div>
+      </div>
+      <div class="hero__background">
+        <img class="hero__photo"
+          src="img/${this._img}.jpg"
+          width="1920" height="695"
+          alt="${this._title}"
+        >
+      </div>
+    </div>`.trim();
+  }
+
+  get id() {
+    return this._id;
+  }
+
+  bind() {
+    this._element.querySelectorAll('.hero__control').forEach((it) => {
+      it.addEventListener('click', this._onToggleBannerClick)
+    });
+  }
+
+  unbind() {
+    this._element.querySelectorAll('.hero__control').forEach((it) => {
+      it.removeEventListener('click', this._onToggleBannerClick)
+    });
+  }
+}
+
 class Model extends Component {
   constructor(data) {
     super();
     this._name = data.name || '';
     this._img = data.img || '';
 
-    this._defaultOpen = data.defaultOpen || false,
+    this._defaultOpen = data.defaultOpen || false;
 
     this._onModel = null;
     this._onModelClick = this._onModelClick.bind(this);
@@ -259,9 +364,13 @@ class Car extends Component {
   }
 }
 
-const doFilterCars = (carsData, modelName) => {
-  return carsData.filter((it) => {
-    return it.name.includes(modelName);
+/* Renders */
+
+const renderHeroBanners = (container, bannersData) => {
+  container.innerHTML = '';
+  bannersData.forEach((it) => {
+    const heroBannerElement = new HeroBanner(it, bannersData.length);
+    container.append(heroBannerElement.render());
   });
 }
 
@@ -297,4 +406,5 @@ const renderCatalog = (catalogData) => {
   renderCars(Container.CATALOG_LIST, catalogData.cars);
 };
 
+renderHeroBanners(Container.HERO, Data.BANNERS);
 renderCatalog(Data.CATALOG);
